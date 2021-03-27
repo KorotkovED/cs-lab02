4f8007ecd94a5c86f11657bd3412b3e777269541
Alice <Alice@gmail.com>
2021-03-27 13:04:58 +0300

    code: изменения4

--- main.cpp
+++ main.cpp
@@ -12,13 +12,5 @@ int main()
     cout <<" A - B = " << a - b << '\n';
     cout <<" A * B = " << a * b << '\n';
     cout <<" A / B = " << a / b << '\n';
-    if (a>b)
-    {
-        cout<<"max = "<<a<<'\n';
-    }
-    else
-    {
-        cout<<"max = "<<b<<'\n';
-    }
     return 0;
 }
